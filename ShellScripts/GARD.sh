3e1bd4b6be82664d1a5d5b189e1708b1961b57c7
Data Monkey <datamonkey@silverback.(none)>
2013-05-10 01:14:32 -0700

    FUBAR.sh fix
    Former-commit-id: 3546e17612b87583f9feebe602b4835c2a0d14f3

--- ShellScripts/GARD.sh
+++ ShellScripts/GARD.sh
@@ -1,9 +1,6 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  pwd )"
-. $DIR/Globals.sh
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
-rm -f  $ABS_DIR/Analyses/GARD/spool/$1*progress
-rm -f  $ABS_DIR/Analyses/GARD/spool/$1*out
+rm -f /home/datamonkey/Datamonkey/Analyses/GARD/spool/$1*progress
+rm -f /home/datamonkey/Datamonkey/Analyses/GARD/spool/$1*out
 
 #filename
 #datatype (0 for nuc, 1 for prot)
@@ -13,4 +10,4 @@ rm -f  $ABS_DIR/Analyses/GARD/spool/$1*out
 #number of rate classes
 
 
-(echo $1; echo $2; echo $3; echo $4; echo $5; echo $6) | mpirun -np 61 -exclude $EXCLUDE_NODES /usr/local/bin/HYPHYMPI  USEPATH=/dev/null  $ABS_DIR/Analyses/GARD/GARD.bf  >  $ABS_DIR/Analyses/GARD/hpout 2>&1 &
+(echo $1; echo $2; echo $3; echo $4; echo $5; echo $6) | mpirun -np 65 -exclude 0:1:2:3:4:5:32:33:34:35:36 /usr/local/bin/HYPHYMPI  USEPATH=/dev/null /home/datamonkey/Datamonkey/Analyses/GARD/GARD.bf  > Analyses/GARD/hpout 2>&1 &
--- ShellScripts/PRIME_wrapper.sh
+++ ShellScripts/PRIME_wrapper.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  pwd )"
 #genetic code
 #posterior p
 
-BASEPATH=/Analyses/PRIME/
+BASEPATH=$ABS_DIR/Analyses/PRIME/
 
 (echo $1; echo $2) | /usr/local/bin/HYPHYMP ${BASEPATH}PRIME_DOWNLOAD.bf >  ${BASEPATH}hpout 2>&1
 (echo $1; echo $3;) | /usr/bin/bpsh `beomap --nolocal --exclude $EXCLUDE_NODES` /usr/local/bin/HYPHYMP ${BASEPATH}PRIME_FITGLOBAL.bf  > ${BASEPATH}hpout 2>&1
