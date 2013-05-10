3546e17612b87583f9feebe602b4835c2a0d14f3
Data Monkey <datamonkey@silverback.(none)>
2013-05-10 01:14:32 -0700

    FUBAR.sh fix

--- ShellScripts/FUBAR_wrapper.sh
+++ ShellScripts/FUBAR_wrapper.sh
@@ -6,10 +6,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  pwd )"
 #genetic code
 #posterior p
 
-BASEPATH= $ABS_DIR/Analyses/FUBAR/
+BASEPATH=$ABS_DIR/Analyses/FUBAR/
 
 (echo $1; echo $2) | /usr/local/bin/HYPHYMP ${BASEPATH}FUBAR_Stage0.bf >  $ABS_DIR/Analyses/FUBAR/hpout 2>&1
-(echo $1) | /usr/bin/bpsh `beomap --nolocal` /usr/local/bin/HYPHYMP ${BASEPATH}FUBAR_Stage1.bf  >  $ABS_DIR/Analyses/FUBAR/hpout 2>&1
-(echo $1; echo $3;) | mpirun -map `beomap --all-nodes` /usr/local/bin/HYPHYMPI ${BASEPATH}FUBAR_Stage2.bf > $ABS_DIR/Analyses/FUBAR/hpout 2>&1
-(echo $1; echo 10;) | mpirun -map `beomap -np 10` /usr/local/bin/HYPHYMPI ${BASEPATH}FUBAR_Stage3.bf > $ABS_DIR/Analyses/FUBAR/hpout 2>&1
-(echo $1; echo $4; echo $2; echo 10; echo $3; ) | mpirun -map `beomap --all-nodes`  /usr/local/bin/HYPHYMPI ${BASEPATH}FUBAR_Stage4.bf  > $ABS_DIR/Analyses/FUBAR/hpout 2>&1
+(echo $1) | /usr/bin/bpsh `beomap --nolocal -exclude $EXCLUDE_NODES` /usr/local/bin/HYPHYMP ${BASEPATH}FUBAR_Stage1.bf  >  $ABS_DIR/Analyses/FUBAR/hpout 2>&1
+(echo $1; echo $3;) | mpirun -exclude $EXCLUDE_NODES /usr/local/bin/HYPHYMPI ${BASEPATH}FUBAR_Stage2.bf > $ABS_DIR/Analyses/FUBAR/hpout 2>&1
+(echo $1; echo 10;) | mpirun -map `beomap -np 10 -exclude $EXCLUDE_NODES` /usr/local/bin/HYPHYMPI ${BASEPATH}FUBAR_Stage3.bf > $ABS_DIR/Analyses/FUBAR/hpout 2>&1
+(echo $1; echo $4; echo $2; echo 10; echo $3; ) | mpirun -exclude $EXCLUDE_NODES /usr/local/bin/HYPHYMPI ${BASEPATH}FUBAR_Stage4.bf  > $ABS_DIR/Analyses/FUBAR/hpout 2>&1
