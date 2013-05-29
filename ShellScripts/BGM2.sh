5a608365bcef3ddcc71bad4576fefcd346d65707
Sergei Pond <spond@ucsd.edu>
2013-05-29 11:26:16 -0700

    GARD.sh now working again

--- ShellScripts/BGM2.sh
+++ ShellScripts/BGM2.sh
@@ -6,4 +6,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 rm -f $ABS_DIR/Analyses/BGM/$1.out
 rm -f $ABS_DIR/Analyses/BGM/$1.progress
 
-(echo $1; echo $2; echo $3) | mpirun -np 51 -exclude $EXCLUDE_NODES/usr/local/bin/HYPHYMPI USEPATH=$ABS_DIR/Analyses/BGM/ $ABS_DIR/Analyses/BGM/BGM2.bf > $ABS_DIR/Analyses/BGM/hpout 2>&1 &
+(echo $1; echo $2; echo $3) | mpirun -np 51 -exclude $EXCLUDE_NODES /usr/local/bin/HYPHYMPI USEPATH=$ABS_DIR/Analyses/BGM/ $ABS_DIR/Analyses/BGM/BGM2.bf > $ABS_DIR/Analyses/BGM/hpout 2>&1 &
--- ShellScripts/GARD.sh
+++ ShellScripts/GARD.sh
@@ -1,7 +1,7 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
 rm -f /home/datamonkey/Datamonkey/Analyses/GARD/spool/$1*progress
 rm -f /home/datamonkey/Datamonkey/Analyses/GARD/spool/$1*out
-
+. $DIR/Globals.sh
 #filename
 #datatype (0 for nuc, 1 for prot)
 #model description
@@ -10,4 +10,4 @@ rm -f /home/datamonkey/Datamonkey/Analyses/GARD/spool/$1*out
 #number of rate classes
 
 
-(echo $1; echo $2; echo $3; echo $4; echo $5; echo $6) | mpirun -np 65 -exclude 0:1:2:3:4:5:32:33:34:35:36 /usr/local/bin/HYPHYMPI  USEPATH=/dev/null /home/datamonkey/Datamonkey/Analyses/GARD/GARD.bf  > Analyses/GARD/hpout 2>&1 &
+(echo $1; echo $2; echo $3; echo $4; echo $5; echo $6) | mpirun -np 65 -exclude $EXCLUDE_NODES /usr/local/bin/HYPHYMPI  USEPATH=/dev/null /home/datamonkey/Datamonkey/Analyses/GARD/GARD.bf  > Analyses/GARD/hpout 2>&1 &
