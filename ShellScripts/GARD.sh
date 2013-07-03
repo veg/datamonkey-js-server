b7a006cccfb6ace52d61232fae372403040e07be
UCSD BIT Core <bitcore@ucsd.edu>
2013-07-02 23:09:01 -0700

    Fixed an incorrect call to FUBAR stage 2

--- ShellScripts/GARD.sh
+++ ShellScripts/GARD.sh
@@ -1,9 +1,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &&  pwd )"
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
-rm -f $ABS_DIR/GARD/spool/$1*progress
-rm -f $ABS_DIR/GARD/spool/$1*out
 . $DIR/Globals.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/
+rm -f $ABS_DIR/Analyses/GARD/spool/$1*progress
+rm -f $ABS_DIR/Analyses/GARD/spool/$1*out
 #filename
 #datatype (0 for nuc, 1 for prot)
 #model description
@@ -12,4 +11,4 @@ rm -f $ABS_DIR/GARD/spool/$1*out
 #number of rate classes
 
 
-(echo $1; echo $2; echo $3; echo $4; echo $5; echo $6) | mpirun -np 65 -exclude $EXCLUDE_NODES /usr/local/bin/HYPHYMPI  USEPATH=/dev/null /home/datamonkey/Datamonkey/Analyses/GARD/GARD.bf  > Analyses/GARD/hpout 2>&1 &
+(echo $1; echo $2; echo $3; echo $4; echo $5; echo $6) | mpirun -np 65 -exclude $EXCLUDE_NODES /usr/local/bin/HYPHYMPI  USEPATH=/dev/null $ABS_DIR/Analyses/GARD/GARD.bf  > $ABS_DIR/Analyses/GARD/hpout 2>&1 &
