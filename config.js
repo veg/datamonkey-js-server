36126093cb5b033b2051df506ae4ab5b45015042
Steven Weaver <weaverst@gmail.com>
2013-08-19 17:10:23 -0700

    adding socket.io proxy support. Added event listeners

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {}
+
+config.port = 7000;
+config.output_dir = '/home/sweaver/datamonkey-server-js/hivcluster/output/';
+config.hivnetworkcsv = '/usr/local/bin/hivnetworkcsv';
+config.tn93dist = '/usr/local/bin/TN93dist';
+config.bam2msa = '/opt/share/python3.3/bam2msa';
+config.bealign = '/opt/share/python3.3/bealign';
+
+
+module.exports = config;
