2bb02c613d0c5fc651b4bc1c70a2c9cab044e986
Steven Weaver <weaverst@gmail.com>
2013-08-26 17:06:49 -0700

    Adding status updates
    Former-commit-id: 409f46b13a15a512b600559b706834d975ad184f

--- server.js
+++ server.js
@@ -1,32 +1,67 @@
-// note, io.listen(<port>) will create a http server for ,ou
+/*
+
+  Datamonkey - An API for comparative analysis of sequence alignments using state-of-the-art statistical models.
+
+  Copyright (C) 2013
+  Sergei L Kosakovsky Pond (spond@ucsd.edu)
+  Steven Weaver (sweaver@ucsd.edu)
+
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
 var config = require('./config.js'),
     io = require('socket.io').listen(config.port),
     spawn_job = require('./hivcluster/spawn_job.js');
     
+// For every new connection...
 io.sockets.on('connection', function (socket) {
-  io.sockets.emit('this', { will: 'be received by everyone'});
 
-  socket.emit('connected', { hello: 'world' });
+  // Acknowledge new connection
+  socket.emit('connected', { hello: 'Ready to serve' });
 
+  // A job has been spawned by datamonkey, let's go to work
   socket.on('spawn', function (hiv_cluster_params) {
 
+    // Setup Analysis
     var cluster_analysis = new spawn_job.DoHivClusterAnalysis();
 
-    cluster_analysis.on('status update', function(msg) {
-      socket.emit('status update', msg);
+    // On status updates, report to datamonkey-js
+    cluster_analysis.on('status update', function(status_update) {
+      socket.emit('status update', status_update);
     });
 
-    cluster_analysis.on('completed', function(msg) {
-      socket.emit('completed', msg);
+    // When the analysis completes, return the results to datamonkey.
+    cluster_analysis.on('completed', function(results) {
+      // Send cluster and graph information
+      socket.emit('completed', results);
     });
-
+    
+    // Setup has been completed, run the job with the parameters from datamonkey
     cluster_analysis.run(hiv_cluster_params);
 
   });
-
+  
+  // Log which user disconnected
   socket.on('disconnect', function () {
     io.sockets.emit('user disconnected');
   });
-
 
 });
