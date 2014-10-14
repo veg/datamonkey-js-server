2d4f510129e3f2603b7ade66762745fbc7078a9c
anthony-aylward <aaylward@eng.ucsd.edu>
2014-10-14 08:29:29 -0700

    Add timer and exporting to jobstatus.js

--- jobstatus.js
+++ jobstatus.js
@@ -2,48 +2,74 @@
 //                          qstat job watching module                         //
 //----------------------------------------------------------------------------//
 
-// Accepts a qstat job ID as an argument and returns the job's status.
+// Accepts a qstat job ID as an argument and watches the job by checking its 
+// qstat status once per second.
 
 // Usage: node jobstatus.js job_id
 
 //----------------------------------------------------------------------------//
 
-// Declare some variables, including the child process.
+// Define the job-watching function.
 
-var spawn = require('child_process').spawn,
-    job_id = process.argv[2],
-    qstat = spawn('qstat', [job_id]),
-    status = {
-        C:'Completed',
-        E:'Error',
-        H:'Hold',
-        Q:'Queued',
-        R:'Running'
-    };
+function watchJob(job_id) {
+    metronome = setInterval(returnJobStatus, 1000, job_id);
+};
 
-// If the job exists, check and return its status.
+// Define the status-returning function.
 
-qstat.stdout.on('data', function (data) {
-    var re = /(\s)+/g,
-        job_status = data
-                    .toString()
-                    .split("\n")[2]
-                    .replace(re, " ")
-                    .split(" ")[4];
-    console.log(status[job_status]);
-});
+function returnJobStatus(job_id) {
 
-// If the job doesn't exist, return that information or some other error.
+    // Declare some variables, including the child process.
 
-qstat.stderr.on('data', function (data) {
-    var doesnt_exist = data
-                      .toString()
-                      .slice(0,27)
-                      ==
-                      'qstat: Unknown Job Id Error';
-    if (doesnt_exist) {
-        console.log('Doesn\'t Exist');
-    } else {
-        console.log('stderr: ' + data);
-    };
-}); 
+    var spawn = require('child_process').spawn,
+        qstat = spawn('qstat', [job_id]),
+        status = {
+            C:'Completed',
+            E:'Error',
+            H:'Hold',
+            Q:'Queued',
+            R:'Running'
+        };
+
+    // If the job exists, check and return its status. If it is complete, stop
+    // the metronome.
+
+    qstat.stdout.on('data', function (data) {
+        var re = /(\s)+/g,
+            job_status = data
+                        .toString()
+                        .split("\n")[2]
+                        .replace(re, " ")
+                        .split(" ")[4];
+        if (job_status == 'C') {
+            clearInterval(metronome);
+        };
+        console.log(status[job_status]);
+    });
+
+    // If the job doesn't exist, return that information and stop the metronome.
+    // If some other error occurs, say so.
+
+    qstat.stderr.on('data', function (data) {
+        var doesnt_exist = data
+                          .toString()
+                          .slice(0,27)
+                          ==
+                          'qstat: Unknown Job Id Error';
+        if (doesnt_exist) {
+            clearInterval(metronome);
+            console.log('Doesn\'t Exist');
+        } else {
+            console.log('stderr: ' + data);
+        };
+    });
+};
+
+// Watch a job whose ID was given as an argument.
+
+watchJob(process.argv[2]);
+
+// Export.
+
+exports.watchJob = watchJob;
+exports.returnJobStatus = returnJobStatus;
