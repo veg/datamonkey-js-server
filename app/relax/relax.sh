4f87e8b9ff130f2a09eca649ea5ed90df103bcd4
Steven Weaver <sweaver@ucsd.edu>
2015-02-02 17:12:35 -0800

    check number of annotated branches before submitting to RELAX.bf

--- app/relax/relax.sh
+++ app/relax/relax.sh
@@ -12,9 +12,16 @@ GENETIC_CODE=$genetic_code
 ANALYSIS_TYPE=$analysis_type
 HYPHY=$CWD/../../node_modules/hyphy/HYPHYMP
 RELAX=$CWD/RELAX.bf
+GETCOUNT=$CWD/../../lib/getAnnotatedCount.bf 
 export HYPHY_PATH=$CWD/../../node_modules/hyphy/res/
 
 trap 'echo "Error" > $STATUS_FILE; exit 1' ERR
-echo '(echo '$GENETIC_CODE'; echo '$FN'; echo '$TREE_FN'; echo "2";echo '$ANALYSIS_TYPE') | '$HYPHY' '$RELAX''
-(echo $GENETIC_CODE; echo $FN; echo $TREE_FN; echo "2";echo $ANALYSIS_TYPE) | $HYPHY $RELAX
-echo "Completed" > $STATUS_FILE
+count=$(echo '(echo '$TREE_FN') | '$HYPHY' '$GETCOUNT'' 2> /dev/null)
+if [ $count -eq 2]
+then
+  echo '(echo '$GENETIC_CODE'; echo '$FN'; echo '$TREE_FN'; echo 2;echo '$ANALYSIS_TYPE') | '$HYPHY' '$RELAX''
+  (echo $GENETIC_CODE; echo $FN; echo $TREE_FN; echo 2;echo $ANALYSIS_TYPE) | $HYPHY $RELAX
+else
+  echo '(echo '$GENETIC_CODE'; echo '$FN'; echo '$TREE_FN'; echo 3; echo 2;echo '$ANALYSIS_TYPE') | '$HYPHY' '$RELAX''
+  (echo $GENETIC_CODE; echo $FN; echo $TREE_FN; echo 3; echo 2;echo $ANALYSIS_TYPE) | $HYPHY $RELAX
+fi
