44b52032f94a12025aa0aad379caf02d776caf58
Steven Weaver <sweaver@temple.edu>
2017-09-06 15:21:33 -0400

    removing old files

--- ShellScripts/Globals.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Absolute directory path
-ABS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )"
-
-#Excluded Nodes
-EXCLUDE_NODES="0:1:2:3:4:5:6:7"
-HOSTFILE="$DIR/hosts"
