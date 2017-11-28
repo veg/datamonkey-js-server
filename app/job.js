ffbfe58a388e74bebabe13b679a6343ecaa52416
Steven Weaver <sweaver@temple.edu>
2017-11-28 17:24:39 -0500

    updating datamonkey server logging

--- app/job.js
+++ app/job.js
@@ -1,32 +1,3 @@
-/*
-
-  Datamonkey - An API for comparative analysis of sequence alignments using state-of-the-art statistical models.
-
-  Copyright (C) 2015
-  Sergei L Kosakovsky Pond (spond@ucsd.edu)
-  Steven Weaver (sweaver@ucsd.edu)
-
-  Permission is hereby granted, free of charge, to any person obtaining a
-  copy of this software and associated documentation files (the
-  "Software"), to deal in the Software without restriction, including
-  without limitation the rights to use, copy, modify, merge, publish,
-  distribute, sublicense, and/or sell copies of the Software, and to
-  permit persons to whom the Software is furnished to do so, subject to
-  the following conditions:
-
-  The above copyright notice and this permission notice shall be included
-  in all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-*/
-
 var config = require("../config.json"),
   spawn = require("child_process").spawn,
   fs = require("fs"),
@@ -145,7 +116,6 @@ jobRunner.prototype.submit = function(params, cwd) {
   var self = this;
 
   var qsub = spawn("qsub", params, { cwd: cwd });
-  console.log(params);
 
   qsub.stderr.on("data", function(data) {
     winston.info(data.toString("utf8"));
