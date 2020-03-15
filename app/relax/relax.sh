6423e9321d489992881c2882ffab70a1773d2aa2
Sergei Pond <spond@temple.edu>
2020-03-15 10:31:23 -0400

    Second take at #1115
    Of course keyword arguments have to be on the command line, not piped the stdin

--- app/relax/relax.sh
+++ app/relax/relax.sh
@@ -14,7 +14,6 @@ OMEGA_RATE_CLASSES=3
 
 HYPHY=$CWD/../../.hyphy/HYPHYMP
 RESULT_FILE=$fn.RELAX.json
-GETCOUNT=$CWD/../../lib/getAnnotatedCount.bf
 
 export HYPHY_PATH=$CWD/../../.hyphy/res/
 RELAX=$HYPHY_PATH/TemplateBatchFiles/SelectionAnalyses/RELAX.bf
@@ -22,11 +21,5 @@ RELAX=$HYPHY_PATH/TemplateBatchFiles/SelectionAnalyses/RELAX.bf
 trap 'echo "Error" > $STATUS_FILE; exit 1' ERR
 count=$(echo '(echo '$TREE_FN') | '$HYPHY' '$GETCOUNT'' 2> /dev/null)
 
-if [ $count -eq 2]
-then
-  echo '(echo '$GENETIC_CODE'; echo '$FN'; echo '$TREE_FN'; echo 2; echo '$OMEGA_RATE_CLASSES'; echo '$ANALYSIS_TYPE'; echo '$RESULTS_FILE';) | '$HYPHY' -i LIBPATH='$HYPHY_PATH' '$RELAX''
-  (echo $GENETIC_CODE; echo $FN; echo $TREE_FN; echo 2; echo $OMEGA_RATE_CLASSES; echo $ANALYSIS_TYPE; echo $RESULTS_FILE;) | $HYPHY -i LIBPATH=$HYPHY_PATH $RELAX > $PROGRESS_FILE
-else
-  echo '(echo '$GENETIC_CODE'; echo '$FN'; echo '$TREE_FN'; echo 3; echo 2; echo '$OMEGA_RATE_CLASSES'; echo '$ANALYSIS_TYPE'; echo '$RESULTS_FILE';) | '$HYPHY' -i LIBPATH='$HYPHY_PATH' '$RELAX''
-  (echo $GENETIC_CODE; echo $FN; echo $TREE_FN; echo 3; echo 2; echo $OMEGA_RATE_CLASSES; echo $ANALYSIS_TYPE; echo $RESULTS_FILE;) | $HYPHY -i LIBPATH=$HYPHY_PATH $RELAX > $PROGRESS_FILE
-fi
+ echo '(echo '$GENETIC_CODE'; echo '$FN'; echo '$TREE_FN'; echo '$OMEGA_RATE_CLASSES'; echo '$ANALYSIS_TYPE'; ) | '$HYPHY' -i LIBPATH='$HYPHY_PATH' '$RELAX' --test TEST --reference REFERENCE --output $RESULT_FILE'
+ (echo $GENETIC_CODE; echo $FN; echo $TREE_FN; echo $OMEGA_RATE_CLASSES; echo $ANALYSIS_TYPE; ) | $HYPHY -i LIBPATH=$HYPHY_PATH $RELAX --test TEST --reference REFERENCE --output $RESULT_FILE > $PROGRESS_FILE
